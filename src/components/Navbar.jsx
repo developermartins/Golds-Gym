d77723b8ba39fad7833983584786fd88dc28d363
martsDev <martssilvapro@gmail.com>
2022-08-11 17:38:03 -0300

    Add imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+
 const Navbar = () => {
   return (
     <div>Navbar</div>
