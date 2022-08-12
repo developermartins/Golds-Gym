1421fa221bdb346c47d6b973e7ccaf9457275f69
martsDev <martssilvapro@gmail.com>
2022-08-12 19:15:38 -0300

    implements BodyPart

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -19,12 +19,24 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
             cursor: 'pointer',
             gap: '47px',
         }}
+        onClick={() => { 
+            setBodyPart(item)
+            window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' })
+        }}
     >
         <img 
             src={ Icon }
             alt="dumbbell"
-            styles={{ with: '40px', height: '40px' }}
+            style={{ with: '40px', height: '40px' }}
         />
+        <Typography
+          fontSize="24px"
+          fontWeight="bold"
+          color="#3A1212"
+          textTransform="capitalize"  
+        >
+            { item }
+        </Typography>
     </Stack>
   );
 };
