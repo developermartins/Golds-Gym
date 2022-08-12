24bc52fad3dc590c7fd311217bcd4f4895e0476a
martsDev <martssilvapro@gmail.com>
2022-08-12 17:48:21 -0300

    setup exercises api

---
+++ src/utils/fetchData.js
@@ -0,0 +1,16 @@
+export const exerciseOptions = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+
+    const data = await res.json();
+
+    return data;
+};
