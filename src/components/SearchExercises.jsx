f65fdd90b4696699acfa0fc78e6e89c64d3e81cf
martsDev <martssilvapro@gmail.com>
2022-08-12 17:38:59 -0300

    add searchData.js

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
+
+  const [search, setSearch] = useState('');
+
+  const handleSearch = async () => {
+    if (search) {
+      // const exerciseData = await fetchData();
+    };
+  };
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
@@ -29,8 +38,8 @@ const SearchExercises = () => {
             borderRadius: '40px'
           }}
           height="76px"
-          value=""
-          onChange={(e) => {}}
+          value={ search }
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
@@ -52,6 +61,7 @@ const SearchExercises = () => {
             position: "absolute",
             right: '0'
           }}
+          onClick={ handleSearch }
         >
           Search
         </Button>
