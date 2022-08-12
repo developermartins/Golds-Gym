82f20fc3472895e1731e0bae616e7a2a8843a25e
martsDev <martssilvapro@gmail.com>
2022-08-12 18:53:25 -0300

    implements BodyParts card

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Stack, Typography } from '@mui/material';
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png';
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+        type="button"
+        alignItems="center"
+        justifyContent="center"
+        className="bodyPart-card"
+        sx={{
+            borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+            backgroundColor: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '280px',
+            cursor: 'pointer',
+            gap: '47px',
+        }}
+    >
+        <img 
+            src={ Icon }
+            alt="dumbbell"
+            styles={{ with: '40px', height: '40px' }}
+        />
+    </Stack>
   );
 };
 
