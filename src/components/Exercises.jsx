968c33f0faa50c893d803a78ead4dd4e4710f46c
martsDev <martssilvapro@gmail.com>
2022-08-15 15:00:22 -0300

    import ExerciseCard

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -4,6 +4,8 @@ import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 
+import ExerciseCard from './ExerciseCard';
+
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   console.log(exercises)
