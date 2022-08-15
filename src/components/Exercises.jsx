fc0a98aac33d9e3154dacfa687cd64d8a29baf39
martsDev <martssilvapro@gmail.com>
2022-08-15 16:57:57 -0300

    implements paginate functions

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -8,7 +8,23 @@ import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
-  console.log(exercises)
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth' });
+
+  };
 
   return (
     <Box id="exercises"
@@ -28,7 +44,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        { exercises.map((exercise, index) => (
+        { currentExercises.map((exercise, index) => (
           <ExerciseCard key={ index } exercise={ exercise } />
         )) }
       </Stack>
@@ -38,7 +54,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
               color="standard"
               shape="rounded"
               defaultPage={ 1 }
-              count={ Math.ceil(exercises.length / 9) }
+              count={ Math.ceil(exercises.length / exercisesPerPage) }
               page={ currentPage }
               onChange={ paginate }
               size="large"
