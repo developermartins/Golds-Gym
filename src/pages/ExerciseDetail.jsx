0a2bdd5b250ecf9ef25d70630f2b77f4682d55ec
martsDev <martssilvapro@gmail.com>
2022-08-16 10:55:15 -0300

    implements fetch data in ExerciseDetail

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -3,10 +3,34 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercise from '../components/SimilarExercise';
+
 const ExerciseDetail = () => {
+
+  const [exerciseDetail, setExerciseDetail] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+
+
+
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
   return (
     <Box>
-      <Detail />
+      <Detail exerciseDetail={ exerciseDetail } />
       <ExerciseVideos />
       <SimilarExercise />
     </Box>
