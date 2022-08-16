9fb0ef7a52edb7906d55ad493c38ec08f77580a7
martsDev <martssilvapro@gmail.com>
2022-08-16 18:25:41 -0300

    fix bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <Box mt="80px" bgcolor="#fff3f4">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={ Logo } alt="logo" width="200px" height="40px" />
-        <Typography variant='h5' pb="40px" mt="20px">
+        <Typography variant='h6' pb="40px" mt="20px">
           Developed by Lucas Silva
         </Typography>
       </Stack>
