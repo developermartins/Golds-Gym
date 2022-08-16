e151bba47b6ce8330ed46d2cd867947b833a5e47
martsDev <martssilvapro@gmail.com>
2022-08-16 12:04:04 -0300

    implements Detail component

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -7,9 +7,22 @@ import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({ exerciseDetail }) => {
 
-    console.log(exerciseDetail);
-
     const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
+    const extraDetails = [
+        {
+            icon: BodyPartImage,
+            name: bodyPart,
+        },
+        {
+            icon: TargetImage,
+            name: target,
+        },
+        {
+            icon: EquipmentImage,
+            name: equipment,
+        },
+    ];
 
   return (
     <Stack
@@ -22,6 +35,41 @@ const Detail = ({ exerciseDetail }) => {
         }}
     >
         <img src={gifUrl} alt={name} Loading="lazy" className='detail-image' />
+        <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+            <Typography variant='h3'>
+                { name }
+            </Typography>
+            <Typography variant='h6'>
+                Exercises keep you strong. { name } { ' ' }
+                is one of the best exercises to target your { target }. 
+                It will help you improve your mood and gain energy.
+            </Typography>
+            { extraDetails.map((item) => (
+                <Stack 
+                    key={ item.name }
+                    direction="row"
+                    gap="24px"
+                    alignItems="center"
+                >
+                    <Button sx={{ 
+                        background: '#fff2db',
+                        borderRadius: '50%',
+                        width: '100px',
+                        height: '100px' 
+                        }}
+                    >
+                        <img 
+                            src={ item.icon }
+                            alt={ bodyPart }
+                            style={{ width: '50px', height: '50px' }}
+                        />
+                    </Button>
+                    <Typography textTransform="capitalize" variant='h5'>
+                        { item.name }
+                    </Typography>
+                </Stack>
+            )) }
+        </Stack>
     </Stack>
   );
 };
